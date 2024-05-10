a61c37f04870186b50ab04d0660a1820e76b423f
david.1731 <davidmendezrosa@gmail.com>
2024-05-10 01:11:31 +0000

    cancelar citas

--- verCitas.php
+++ verCitas.php
@@ -1,40 +1,83 @@
-<?php
-//var_dump($_POST);
-include 'index.php';
-$clientID = isset($_POST['clientID']) ? $conn->real_escape_string($_POST['clientID']) : 'default_client_id';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registration Confirmation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Tus Citas</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Nombre/th>
+                    <th>Entrenador</th>
+                    <th>Servicio</th>
+                    <th>Nivel</th>
+                    <th>Fecha de Cita</th>
+                    <th>Hora de Cita</th>
+                    <th>Cancelar Cita</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                include 'index.php';
+                $clientID = isset($_POST['clientID']) ? $conn->real_escape_string($_POST['clientID']) : 'default_client_id';
+                
+                $sql = "SELECT
+                cl.firstName AS ClientFirstName,
+                cl.lastName AS ClientLastName,
+                tr.trainerName AS TrainerName,
+                tr.trainerLastName AS TrainerLast,
+                se.serviceName AS ServiceName,
+                le.level AS LevelName,
+                ts.startTime AS AppointmentStartTime,
+                ts.endTime AS AppointmentEndTime,
+                ap.appDate AS AppointmentDate
+                FROM
+                appointmentSlots ap
+                JOIN clients cl ON ap.clientID = cl.clientID
+                JOIN trainers tr ON ap.trainerID = tr.trainerID
+                JOIN service se ON ap.serviceID = se.serviceID
+                JOIN levels le ON ap.levelID = le.levelID
+                JOIN timeSlots ts ON ap.slotID = ts.slotID
+                WHERE
+                cl.clientID = '$clientID'
+                ORDER BY
+                ap.appDate, ts.startTime";
+                
+                $res = mysqli_query($conn, $sql);
+                
+                while($row = mysqli_fetch_assoc($res)) {
+                    $clientFullName = $row['ClientFirstName'] . " " . $row['ClientLastName'];
+                    $trainerFullName = $row['TrainerName'] . " " . $row['TrainerLast'];
+                    $service = $row['ServiceName'];
+                    $level = $row['LevelName'];
+                    $appDate = $row['AppointmentDate'];
+                    $appHour = $row['AppointmentStartTime'] . " - " . $row['AppointmentEndTime'];
+                    
+                    echo "<tr>";
+                    echo "<td>" . $clientFullName ."</td>";
+                    echo "<td>" . $trainerFullName . "</td>";
+                    echo "<td>" . $service . "</td>";
+                    echo "<td>" . $level . "</td>";
+                    echo "<td>" . $appDate . "</td>";
+                    echo "<td>" . $appHour . "</td>";
+                    echo "<td>";
+                    echo "<form action='someAction.php' method='post'>";
+                    echo "<input type='hidden' name='clientId' value='"$clientID"'>";
+                    echo "<button type='submit'>Take Action</button>";
+                    echo "</form>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
 
-$sql = "SELECT
-cl.firstName AS ClientFirstName,
-cl.lastName AS ClientLastName,
-tr.trainerName AS TrainerName,
-se.serviceName AS ServiceName,
-le.level AS LevelName,
-ts.startTime AS AppointmentStartTime,
-ts.endTime AS AppointmentEndTime,
-ap.appDate AS AppointmentDate
-FROM
-appointmentSlots ap
-JOIN clients cl ON ap.clientID = cl.clientID
-JOIN trainers tr ON ap.trainerID = tr.trainerID
-JOIN service se ON ap.serviceID = se.serviceID
-JOIN levels le ON ap.levelID = le.levelID
-JOIN timeSlots ts ON ap.slotID = ts.slotID
-WHERE
-cl.clientID = $clientID
-ORDER BY
-ap.appDate, ts.startTime";
 
-$res = mysqli_query($conn, $sql);
-
-while($row = mysqli_fetch_assoc($res)) {
-    echo "<div>";
-    echo "<h1>Appointment Information</h1>";
-    echo "<p>Client Name: " . $row['ClientFirstName'] . " " . $row['ClientLastName'] . "</p>";
-    echo "<p>Trainer Name: " . $row['TrainerName'] . "</p>";
-    echo "<p>Service Name: " . $row['ServiceName'] . "</p>";
-    echo "<p>Level: " . $row['LevelName'] . "</p>";
-    echo "<p>Appointment Date: " . $row['AppointmentDate'] . "</p>";
-    echo "<p>Appointment Time: " . $row['AppointmentStartTime'] . " - " . $row['AppointmentEndTime'] . "</p>";
-    echo "</div>";
-}
-?>
