7238721548478a1842d64950fed6939503da2d34
david.1731 <davidmendezrosa@gmail.com>
2024-05-12 04:20:17 +0000

    minor changes

--- trainerSide/despliegaHoras.php
+++ trainerSide/despliegaHoras.php
@@ -50,7 +50,7 @@
         </div>
 
         <div class="d-flex flex-column align-items-center">
-            <form action="trainerSignIn.html">
+            <form action="trainerSignIn.php">
                 <button type="submit" class="btn btn-primary">Terminar</button>
             </form>
         </div>
