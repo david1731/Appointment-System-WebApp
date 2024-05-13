23e580b7168a80e9ecc648db80813d383a986dcb
david.1731 <davidmendezrosa@gmail.com>
2024-05-13 20:46:21 +0000

    minor changes

--- clientSide/verCitas.php
+++ clientSide/verCitas.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=0.5">
     <title>Tus Citas</title>
     <link href="crearCuenta.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
